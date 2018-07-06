28a043e8cfaf9bf699a148916ecfaf81f09926f8
geesimon <geesimon@hotmail.com>
2018-07-06 17:37:05 +0800

    first submit

--- task.py
+++ task.py
@@ -1,6 +1,79 @@
 import numpy as np
 from physics_sim import PhysicsSim
 
+class MoveToTask():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, target_pos, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5.):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 1
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+        self.old_pose = self.sim.pose
+
+        # Goal
+        self.target_pos = target_pos 
+
+    def get_distance(self, a, b):
+        return np.linalg.norm(a - b)
+
+    
+    def get_reward(self, rotor_speeds):    
+        done = self.sim.next_timestep(rotor_speeds)
+        if done:
+            """Out of range, give a big punishment"""
+            return (True, -1000)
+
+        new_dist = self.get_distance(self.sim.pose[0:3], self.target_pos)
+        old_dist = self.get_distance(self.old_pose[0:3], self.target_pos)        
+        
+        self.old_pose = self.sim.pose
+        
+        if new_dist < 0.1: 
+            """Reach target, give a big reward"""
+            return (True, 1000)
+        else:
+            """Reward each step on how much closer to the target"""
+            return (False, old_dist - new_dist)
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        all_reward = 0
+        pose_all = []
+        for i in range(self.action_repeat):
+            done, reward = self.get_reward(rotor_speeds)
+            all_reward += reward
+            if done:
+                for _ in range(i, self.action_repeat):
+                    pose_all.append(self.sim.pose)
+                break
+            else:
+                pose_all.append(self.sim.pose)
+        
+        next_state = np.concatenate(pose_all)
+
+        return next_state, all_reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        self.old_pose = self.sim.pose
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
+
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
     def __init__(self, init_pose=None, init_velocities=None, 
